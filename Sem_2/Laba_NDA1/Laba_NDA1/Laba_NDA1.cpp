3edb7da0b02ce52a4c7ec0ed9fb1ba34d606b8a4
DJEsc <xxxdjeschglxxx@gmail.com>
2020-05-28 22:49:38 +0300

    rebase tree

---
+++ Sem_2/Laba_NDA1/Laba_NDA1/Laba_NDA1.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+
+using namespace std;
+
+struct SubUnits {
+	short code;
+	string name;
+	int year;
+	int edu;
+};
+
+void showData(SubUnits* Unit, int amount) {
+	system("cls");
+	cout << "#\t" << "Code\t" << "Name\t" << "Education\t" << "Year" << endl;
+	cout << "==========================================================" << endl;
+	for (int i = 0; i < amount; i++) {
+		cout << i + 1 << "\t" << Unit[i].code << "\t" << Unit[i].name << "\t" << Unit[i].edu << "\t" << Unit[i].year << endl;
+	}
+}
+
+int main() {
+	int amountOfUnits;
+	short coden;
+	cout << "Code for sort:";
+	cin >> coden;
+	cout << "Amount: ";
+	cin >> amountOfUnits;
+	SubUnits* Unit = new SubUnits[amountOfUnits];
+	for (int i = 0; i < amountOfUnits; i++) {
+		cout << i + 1 << " unit\n";
+		cout << "Code: ";
+		cin >> Unit[i].code;
+		cout << "Name: ";
+		cin.get();
+		getline(cin, Unit[i].name);
+		cout << "Birth year: ";
+		cin >> Unit[i].year;
+		cout << "Education: ";
+		cin >> Unit[i].edu;
+	}
+
+	for (int i = 0; i < amountOfUnits - 1; i++) {
+		for (int j = 0; j < amountOfUnits - i - 1; j++) {
+			if (Unit[j].code > Unit[j + 1].code) {
+				swap(Unit[j].code, Unit[j + 1].code);
+				swap(Unit[j].name, Unit[j + 1].name);
+				swap(Unit[j].year, Unit[j + 1].year);
+				swap(Unit[j].edu, Unit[j + 1].edu);
+			}
+		}
+	}
+	for (int i = 0; i < amountOfUnits - 1; i++) {
+		for (int j = 0; j < amountOfUnits - i - 1; j++) {
+			if (Unit[j].code == coden && Unit[j + 1].code == coden) {
+				if (Unit[j].edu < Unit[j + 1].edu) {
+					swap(Unit[j].code, Unit[j + 1].code);
+					swap(Unit[j].name, Unit[j + 1].name);
+					swap(Unit[j].year, Unit[j + 1].year);
+					swap(Unit[j].edu, Unit[j + 1].edu);
+				}
+			}
+		}
+	}
+	for (int i = 0; i < amountOfUnits - 1; i++) {
+		for (int j = 0; j < amountOfUnits - i - 1; j++) {
+			if (Unit[j].code == coden && Unit[j + 1].code == coden) {
+				if (Unit[j].year < Unit[j + 1].year) {
+					swap(Unit[j].code, Unit[j + 1].code);
+					swap(Unit[j].name, Unit[j + 1].name);
+					swap(Unit[j].year, Unit[j + 1].year);
+					swap(Unit[j].edu, Unit[j + 1].edu);
+				}
+			}
+		}
+	}
+	showData(Unit, amountOfUnits);
+}
---
+++ Sem_2/Laba_NK1/Laba_NK1/Laba_NK1.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int n = 5;
+
+struct node {
+	int marks[n];
+	node* Next;
+};
+
+class List {
+	node* Head;
+	int num = 0;
+public:
+	List() {
+		Head = NULL;
+	}
+	~List() {
+		while (Head != NULL) {
+			node* temp = Head->Next;
+			delete Head;
+			Head = temp;
+		}
+	}
+	void Input() {
+		node* temp = new node;
+		for (int i = 0; i < n; i++) {
+			cin >> temp->marks[i];
+		}
+		temp->Next = Head;
+		Head = temp;
+	}
+	void Output() {
+		node* temp = Head;
+		cout << num;
+	}
+	void Count() {
+		bool flag = false;
+		node* temp = new node;
+		for (int i = 0; i < n; i++) {
+			if (temp->marks[i] < 8) {
+				flag = false;
+				break;
+			}
+			else {
+				flag = true;
+			}
+		}
+		if (flag)
+			num++;
+		temp->Next = Head;
+		Head = temp;
+	}
+};
+
+int main() {
+	int N;
+	List lst;
+	cout << "N = ";
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cout << i + 1 << " student marks: ";
+		lst.Input();
+	}
+	for (int i = 0; i < N; i++) {
+		lst.Count();
+	}
+	lst.Output();
+	lst.~List();
+}
