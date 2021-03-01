aa71884dfe1eefee5ee0132fed28963dab8a0637
DJEsc <xxxdjeschglxxx@gmail.com>
2021-03-01 14:32:00 +0300

    update 01.03.2021 ver 2

--- Sem_4/PR_L3_2_N10/src/com/djesc/Line.java
+++ Sem_4/PR_L3_2_N10/src/com/djesc/Line.java
@@ -1,24 +1,59 @@
 package com.djesc;
 
 public class Line {
-    int x;
-    int y;
+    double x1, x2, y1, y2, k;
 
-    Li
-
-    public void setX(int x) {
-        this.x = x;
+    public Line(){
+        super();
     }
 
-    public void setY(int y) {
-        this.y = y;
+    public void setX1(double x1) {
+        this.x1 = x1;
     }
 
-    public int getX() {
-        return x;
+    public void setX2(double x2) {
+        this.x2 = x2;
     }
 
-    public int getY() {
-        return y;
+    public void setY1(double y1) {
+        this.y1 = y1;
+    }
+
+    public void setY2(double y2) {
+        this.y2 = y2;
+    }
+
+    public void setK(double k) {
+        this.k = k;
+    }
+
+    public double getX1() {
+        return x1;
+    }
+
+    public double getX2() {
+        return x2;
+    }
+
+    public double getY1() {
+        return y1;
+    }
+
+    public double getY2() {
+        return y2;
+    }
+
+    public double getK() {
+        return k;
+    }
+
+    @Override
+    public String toString(){
+        return "Line{"
+                + "x1=" + x1
+                + ", x2=" + x2
+                + ", y1=" + y1
+                + ", y2=" + y2
+                + "}";
     }
 }
--- Sem_4/PR_L3_2_N10/src/com/djesc/Main.java
+++ Sem_4/PR_L3_2_N10/src/com/djesc/Main.java
@@ -6,13 +6,30 @@ public class Main {
     public static Line[] lines;
     public static int n;
 
+    public static void CountK(){
+        double k;
+        for(int i = 0; i < n; i++){
+            k = (lines[i].getY2() - lines[i].getY1()) / (lines[i].getX2() - lines[i].getX1());
+            lines[i].setK(k);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Введите количество линий: ");
+        System.out.print("Введите количество прямых: ");
         n = in.nextInt();
         lines = new Line[n];
         for(int i = 0; i < n; i++){
             lines[i] = new Line();
+            System.out.print("Введите координаты двух точек прямой:\nx1 = ");
+            lines[i].setX1(in.nextDouble());
+            System.out.print("x2 = ");
+            lines[i].setX2(in.nextDouble());
+            System.out.print("y1 = ");
+            lines[i].setY1(in.nextDouble());
+            System.out.print("y2 = ");
+            lines[i].setY2(in.nextDouble());
         }
+        
     }
 }
