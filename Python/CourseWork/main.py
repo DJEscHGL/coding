ec3a2bcfc27e1cb9f3c6db665665cb20b8549b05
DJEsc <xxxdjeschglxxx@gmail.com>
2021-05-26 22:13:31 +0300

    update CourseWork

--- Python/CourseWork/main.py
+++ Python/CourseWork/main.py
@@ -1,42 +1,47 @@
 import math
 import tkinter as tk
 
-R = 5.0  # Радиус шара
-V0 = 10.0  # Начальная скорость
-K1 = 0.05  # Коэффициент трения скольжения
-K2 = 0.04  # Коэффициент трения качения
+R = 0.25  # Радиус шара
+V0 = 5.0  # Начальная скорость
+K1 = 0.2  # Коэффициент трения скольжения
+K2 = 0.002  # Коэффициент трения качения
 G = 9.81  # Гравитационная постоянная
-T1 = (2 * R * V0) / (5 * G + 2 * R * K1 * G)
-X1 = V0 * T1 - (K1 * G * T1 * T1) / 2
-V1 = V0 - K1 * G * T1
-T2 = (R * V1) / (K2 * G)
-X2 = V1 * T2 - (K2 * T2 * T2 * G) / (2 * R)
-allX = X1 + X2
-allT = T1 + T2
+T1 = (2 * V0) / (7 * K1 * G)  # Время движения с коэффициентом скольжения
+X1 = (V0 * T1) - ((K1 * G * T1 * T1) / 2)  # Перемещение за время T1
+V1 = V0 - K1 * G * T1  # Скорость в момент T1
+T2 = (R * V1) / (K2 * G)  # Время движения с коэффициентом качения
+X2 = V1 * T2 - (K2 * T2 * T2 * G) / (2 * R)  # Перемещение за время T2
+allX = X1 + X2  # Общее перемещение
+allT = T1 + T2  # Общее время
 
-v = V0
-deltaT = 1
-temp = 0
-t = deltaT * 2
-x = V0 * t - (K1 * G * deltaT * deltaT) / 2
-camX = 0
-deltaCamX = 470
-deltaX = 0
-flag = 0
-tempV = 0
-w = 0
-angle_in_radians = 0
-line_length = 25
-center_x = 0
-center_y = 125
-end_x = 150
-end_y = 125
+v = V0  # Скорость в момент t
+deltaT = 0.1  # Скорость изменения t
+temp = 0  # Счётчик перемещений камеры на 1 page
+t = deltaT * 2  # Текущее время
+x = V0 * t - (K1 * G * deltaT * deltaT) / 2  # Положение в момент t
+camX = 0  # Положение камеры
+deltaCamX = 470  # Перемещение камеры
+deltaX = 0  # Перемещение шара за t
+flag = 0  # Переключатель для Пауза
+w = 0  # Угловая скорость
+angle_in_radians = 0  # Угол поворота
+line_length = 25  # Длина радиуса
+center_x = 0  # Центр шара по X
+center_y = 125  # Центр шара по Y
+end_x = 150  # Конец радиуса по X
+end_y = 125  # Конец радиуса по Y
 
+print(str("T1 = ") + str(T1) + str("\tX1 = ") + str(X1) + str("\tV1 = ") + str(V1))
+print(str("T2 = ") + str(T2) + str("\tX2 = ") + str(X2) + str("\tV2 = ") + str(0))
+print(str("T = ") + str(allT) + str("\tX = ") + str(allX) + "\n")
+
+# Создание окна и установка параметров
 root = tk.Tk()
 root.resizable(width=False, height=False)
 root.geometry('875x240')
 root.title('Моделирование движения шара после удара по негладкой горизонтальной поверхности')
 
+# Расположение элементов окна и canvas
 canvas = tk.Canvas(root, bg="white", width=470, height=235)
 output = tk.Canvas(root, bg="lightgrey", width=400, height=90)
 radius = tk.Entry(root)
@@ -60,15 +65,13 @@ coordinate = output.create_text(100, 50, justify='left')
 omega = output.create_text(100, 70, justify='left')
 circle = canvas.create_oval(100, 100, 150, 150, fill="lightblue")
 line = canvas.create_line(125, 125, 150, 125)
-
-print(str("T1 = ") + str(T1) + str("\tX1 = ") + str(X1) + str("\tV1 = ") + str(V1))
-print(str("T2 = ") + str(T2) + str("\tX2 = ") + str(X2) + str("\tV2 = ") + str(0))
-print(str("T = ") + str(allT) + str("\tX = ") + str(allX) + "\n")
+for i in range(5, 9000, 10):
+    canvas.create_line(i, 150, i, 170)
 
 
-def do_one_frame(self):
-    global v, V1, X1, X2, allX, T1, T2, allT, R, K1, K2, t, circle, deltaX, deltaCamX, x, camX, temp, tempV, output, w
-    global line, angle_in_radians, center_x, center_y, line_length, end_x, end_y
+def do_one_frame(self):  # функция моделирования движения при нажатии кнопки Запуск
+    global v, V1, X1, X2, allX, T1, T2, allT, R, K1, K2, t, circle, deltaX, deltaCamX, x, camX, temp, output, w
+    global line, angle_in_radians, center_x, center_y, line_length, end_x, end_y, flag
     if camX - deltaCamX >= -50:
         self.xview_scroll(1, "pages")
         deltaCamX += 470
@@ -90,22 +93,25 @@ def do_one_frame(self):
     output.itemconfig(coordinate, text=str("x = ") + str(x))
     output.itemconfig(omega, text=str("w = ") + str(w))
     self.delete(line)
-
     center_x = self.coords(circle)[0] + 25
     center_y = 125
-    self.move(circle, deltaX, 0)
+    self.move(circle, deltaX * 10, 0)
     angle_in_radians = w * t * math.pi / 180
 
     end_xx = end_x
     end_x = center_x + (end_x - center_x) * math.cos(angle_in_radians) - (end_y - center_y) * math.sin(angle_in_radians)
-    end_y = center_y + (end_y - center_y) * math.cos(angle_in_radians) + (end_xx - center_x) * math.sin(angle_in_radians)
-    end_x += deltaX
+    end_y = center_y + (end_y - center_y) * math.cos(angle_in_radians) + (end_xx - center_x) * math.sin(
+        angle_in_radians)
+    end_x += (deltaX * 10)
 
-    line = canvas.create_line(center_x + deltaX, 125,  end_x, end_y)
+    line = canvas.create_line(center_x + deltaX * 10, 125, end_x, end_y)
     camX = center_x
     t += deltaT
     if v > 0:
-        self.after(25, do_one_frame, canvas)
+        if flag == 1:
+            flag = 0
+        else:
+            self.after(30, do_one_frame, canvas)
     else:
         output.itemconfig(velocity, text=str("v = ") + str(0))
         output.itemconfig(time, text=str("t = ") + str(allT))
@@ -113,11 +119,11 @@ def do_one_frame(self):
         output.itemconfig(omega, text=str("w = ") + str(0))
 
 
-def _on_mousewheel(event):
-    canvas.xview_scroll(int(-1*(event.delta/120)), "units")
+def _on_mousewheel(event):  # функция для скроллинга canvas вдоль оси X
+    canvas.xview_scroll(int(-1 * (event.delta / 120)), "units")
 
 
-def getvalue():
+def getvalue():  # Функция для кнопки Изменить значения
     global R, V0, K1, K2, v, x, T1, X1, V1, T2, X2, allX, allT
     R = float(radius.get())
     V0 = float(initialVelocity.get())
@@ -142,15 +148,22 @@ def getvalue():
     allT = T1 + T2
 
 
-def reset(self):
+def pause():  # функция для кнопки Пауза
+    global flag
+    flag = 1
+
+
+def reset(self):  # функция для кнопки Сброс
     global v, V0, t, allX, deltaX, deltaCamX, camX, x, line, circle, temp, deltaT, w, center_x, center_y, end_x, end_y
+    global flag
+    pause()
     self.delete(circle)
     self.delete(line)
     circle = canvas.create_oval(100, 100, 150, 150, fill="lightblue")
     line = canvas.create_line(125, 125, 150, 125)
     self.xview_scroll(-temp, "pages")
     v = V0
-    deltaT = 1
+    deltaT = 0.1
     temp = 0
     t = deltaT * 2
     x = V0 * t - (K1 * G * deltaT * deltaT) / 2
@@ -162,10 +175,15 @@ def reset(self):
     center_y = 125
     end_x = 150
     end_y = 125
+    flag = 0
+    output.itemconfig(velocity, text=str("v = ") + str(v))
+    output.itemconfig(time, text=str("t = ") + str(0))
+    output.itemconfig(coordinate, text=str("x = ") + str(0))
+    output.itemconfig(omega, text=str("w = ") + str(w))
 
 
 canvas.bind_all("<MouseWheel>", _on_mousewheel)
-
+# Расположение кнопок и label
 tk.Label(root, text='Радиус шара').place(x=0, y=0)
 tk.Label(root, text='Начальная скорость').place(x=0, y=25)
 tk.Label(root, text='Коэф. трения скольжения').place(x=0, y=50)
@@ -173,7 +191,6 @@ tk.Label(root, text='Коэф. трения качения').place(x=0, y=75)
 tk.Button(root, text='Изменить значения', command=getvalue).place(x=0, y=110)
 tk.Button(root, text='Запуск', command=lambda: do_one_frame(canvas)).place(x=151, y=110)
 tk.Button(root, text='Сброс', command=lambda: reset(canvas)).place(x=209, y=110)
+tk.Button(root, text='Пауза', command=pause).place(x=264, y=110)
 
 root.mainloop()
-
-# print(str("dX = ") + str(deltaX) + str("\tx = ") + str(x) + str("\tv = ") + str(v) + str("\tt = ") + str(t) + "\n")
