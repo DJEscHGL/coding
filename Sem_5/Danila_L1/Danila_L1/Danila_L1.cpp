3981a153fb3e0eea8b394b858a0b54576b680c12
DJEsc <xxxdjeschglxxx@gmail.com>
2022-03-24 20:49:36 +0300

    update 24.03.2022

---
+++ Sem_5/Danila_L1/Danila_L1/Danila_L1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool findNegative(double** arr, int n, int mm) {
+	for (int i = 0; i < n; i++) {
+		if (arr[mm][i] < 0)
+			return true;
+	}
+}
+
+int main() {
+	setlocale(0, "");
+	double sum = 0;
+	double** arr;
+	int n, m;
+	cout << "Введите n = ";
+	cin >> n;
+	cout << "Введите m = ";
+	cin >> m;
+	arr = new double*[n];
+	for (int i = 0; i < n; i++)
+		arr[i] = new double[m];
+	cout << "Заполните массив:\n";
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> arr[i][j];
+		}
+	}
+	for (int i = 0; i < m; i++) {
+		if (findNegative(arr, n, i))
+		{
+			for (int j = 0; j < n; j++) {
+				sum += arr[j][i];
+			}
+			cout << "Сумма столбца " << i << " равна " << sum << endl;
+			sum = 0;
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		delete arr[i];
+	}
+	delete arr;
+}
